5a81c38278404814e8801ac2790a5ff45bec153e
Mgin <mavangin@gmail.com>
2020-11-05 21:52:35 -0800

    bug fixes

--- controllers/coffeeController.js
+++ controllers/coffeeController.js
@@ -2,10 +2,11 @@ let Coffee = require('../models/coffee');
 let Brand = require('../models/brand')
 const { body, validationResult } = require('express-validator')
 let async = require('async')
+var mongoose = require('mongoose');
+
 //let BikeInstance = require('../models/bikeInstance')
 
-exports.coffeeList = function (req, res, err) {
-    console.log(Coffee.find({}))
+exports.coffeeList = function (req, res, next) {
     Coffee.find({}, 'name categories')
         .populate('categories')
         .exec(function (err, coffeeList) {
@@ -16,7 +17,7 @@ exports.coffeeList = function (req, res, err) {
         })
 }
 
-exports.coffeeDetail = function (req, res, err) {
+exports.coffeeDetail = function (req, res, next) {
     Coffee.findById(req.params.id)
         .populate('categories')
         .populate('brand')
@@ -24,12 +25,11 @@ exports.coffeeDetail = function (req, res, err) {
             if (err) {
                 return next(err)
             }
-            console.log(coffeeItem)
             res.render('coffee_detail', { coffeeItem })
         })
 }
 
-exports.coffeeUpdateGet = function (req, res, err) {
+exports.coffeeUpdateGet = function (req, res, next) {
     async.parallel({
         brands: function (callback) {
             Brand.find({}, "name")
@@ -37,9 +37,9 @@ exports.coffeeUpdateGet = function (req, res, err) {
         },
         coffeeItem: function (callback) {
             Coffee.findById(req.params.id)
-            .populate('categories')
-            .populate('brand')
-            .exec(callback)
+                .populate('categories')
+                .populate('brand')
+                .exec(callback)
         },
     }, function (err, results) {
         if (err) {
@@ -47,23 +47,15 @@ exports.coffeeUpdateGet = function (req, res, err) {
         }
         let selectedBrand
         results.brands.forEach(brand => {
-            console.log(brand._id + " first")
-            console.log(results.coffeeItem.brand._id + "   second")
-            console.log(typeof results.coffeeItem.brand._id )
-       
             if (brand._id.toString() == results.coffeeItem.brand._id.toString()) {
                 selectedBrand = brand
             }
         })
-
-
-        console.log(results.brands)
-        console.log("coffeeItem " + results.coffeeItem)
-        res.render('coffeeUpdateForm', { brands : results.brands, coffeeItem : results.coffeeItem, selectedBrand})
+        res.render('coffeeUpdateForm', { brands: results.brands, coffeeItem: results.coffeeItem, selectedBrand })
     })
 }
 
-exports.coffeeUpdatePost = function (req, res, err) {
+exports.coffeeUpdatePost = function (req, res, next) {
     const errors = validationResult(req);
     let coffee = new Coffee(
         {
@@ -88,8 +80,7 @@ exports.coffeeUpdatePost = function (req, res, err) {
 
 
 
-exports.coffeeDeletePost = function (req, res, err) {
-    console.log(req.params.id)
+exports.coffeeDeletePost = function (req, res, next) {
     Coffee.findByIdAndRemove(req.params.id, {}, function (err) {
         if (err) {
             return next(err)
@@ -97,6 +88,40 @@ exports.coffeeDeletePost = function (req, res, err) {
         res.redirect('/catalog/coffee');
     })
 }
+
+exports.coffeeCreateGet = function (req, res, next) {
+    Brand.find({}, "name")
+        .exec(function (err, brands) {
+            if (err) {
+                return next(err)
+            }
+            res.render('coffeeCreateForm', { brands })
+        })
+}
+
+
+
+exports.coffeeCreatePost = function (req, res, next) {
+    console.log()
+    let name = req.body.name
+    let description = req.body.description
+    let categories = req.body.categories
+    let brand = req.body.brand
+    let price = req.body.price
+    let location = req.body.location
+    let stock = req.body.stock
+
+    let coffee = new Coffee({name, description, categories, brand, price, location, stock})
+    coffee.save(function (err) {
+        if (err) {
+            next(err)
+            return
+        }
+        res.redirect('/catalog/coffee');
+
+    })
+}
+
 
 /* exports.bikeInstance = function (req, res, err) {
     BikeInstance.find({}, 'bike')
--- routes/catalog.js
+++ routes/catalog.js
@@ -13,18 +13,22 @@ router.get('/', function(req,res,err) {
 
 //router.get('/category', categoryController.CategoryList)
 
+
 router.get('/coffee', coffeeController.coffeeList);
+
+router.get('/coffee/create', coffeeController.coffeeCreateGet);
+
+router.post('/coffee/create', coffeeController.coffeeCreatePost);
 
 router.get('/coffee/:id', coffeeController.coffeeDetail)
 //router.get('/bikeInstance', coffeeController.bikeInstance);
 
 router.get('/coffee/:id/update', coffeeController.coffeeUpdateGet);
 
-//router.get('/coffee/create', coffeeController.coffeeCreate);
-
 router.post('/coffee/:id/update', coffeeController.coffeeUpdatePost)
 
 router.post('/coffee/:id/delete', coffeeController.coffeeDeletePost)
+
 
 
 router.get('/brand', brandController.brandList);
