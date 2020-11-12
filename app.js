9f2600de4389fb301474db2a321ea9675180fa1d
Mgin <mavangin@gmail.com>
2020-11-11 21:49:29 -0800

    bug fixes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 //Set up mongoose connection
 var mongoose = require('mongoose');
 const router = require('./routes/catalog');
-var mongoDB = MONGODB_URI; 
+var mongoDB = process.env.MONGODB_URI
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
